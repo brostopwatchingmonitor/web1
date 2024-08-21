690a5fe69fc948f449d3c3515070d04ecaf3f1c5
Alif Shidqi <alifshidqiputraamir@gmail.com>
2024-08-21 21:07:33 +0700

    penambahan logo flag

--- resources/views/layoutsadmin/header1.blade.php
+++ resources/views/layoutsadmin/header1.blade.php
@@ -131,8 +131,8 @@
           </div>
           <!-- Megamenu end     -->
           <!-- Languages dropdown    -->
-          <div class="list-inline-item dropdown"><a id="languages" rel="nofollow" data-target="#" href="#" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" class="nav-link language dropdown-toggle"><img src="admincss/img/flags/16/GB.png" alt="English"><span class="d-none d-sm-inline-block">English</span></a>
-            <div aria-labelledby="languages" class="dropdown-menu"><a rel="nofollow" href="#" class="dropdown-item"> <img src="admincss/admincss/img/flags/16/DE.png" alt="English" class="mr-2"><span>German</span></a><a rel="nofollow" href="#" class="dropdown-item"> <img src="admincss/img/flags/16/FR.png" alt="English" class="mr-2"><span>French  </span></a></div>
+          <div class="list-inline-item dropdown"><a id="languages" rel="nofollow" data-target="#" href="#" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" class="nav-link language dropdown-toggle"><img src="admincss/img/flags/16/png-transparent-flag-of-england-flag-of-the-united-kingdom-american-flag-angle-flag-text.png" style="width: 25px"alt="English"><span class="d-none d-sm-inline-block">English</span></a>
+            <div aria-labelledby="languages" class="dropdown-menu"><a rel="nofollow" href="#" class="dropdown-item"> <img src="admincss/img/flags/16/images.png" style="width: 25px"alt="English" class="mr-2"><span>German</span></a><a rel="nofollow" href="#" class="dropdown-item"> <img src="admincss/img/flags/16/Flag_of_France_official.svg.png" style="width: 25px" alt="English" class="mr-2"><span>French  </span></a></div>
           </div>
           <!-- Log out               -->
           <div class="list-inline-item logout">
